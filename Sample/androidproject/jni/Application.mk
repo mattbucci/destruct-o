181d542ce8a597e7697cc068bfcbaaf01482689a
wd40bomber7 <apscience@earthlink.net>
2014-02-03 17:03:33 -0800

    Further progress towards an android build. Draw system overhaul for opengl es 2 pending

--- Sample/androidproject/jni/Application.mk
+++ Sample/androidproject/jni/Application.mk
@@ -1,1 +1,9 @@
-APP_STL:=stlport_static
+
+# use this to select gcc instead of clang
+NDK_TOOLCHAIN_VERSION := 4.8
+
+# then enable c++11 extentions in source code
+APP_CPPFLAGS += -std=c++11
+# or use APP_CPPFLAGS := -std=gnu++11
+
+APP_STL:=gnustl_static
