e2f81b9e18eb1ff237505f4df85d690855364e8b
wd40bomber7 <apscience@live.com>
2014-02-09 19:59:55 -0800

    Fixed a mistake in the previous commit along with fully restoring iOS support

--- Sample/source/GLShader.cpp
+++ Sample/source/GLShader.cpp
@@ -39,9 +39,9 @@ GLShader::GLShader(GLCommonShaderFile * commonShader, string filename,GLenum sha
 	//Copy the virtual shader
 	vector<char> virtualShader;
     if (shaderType == GL_VERTEX_SHADER)
-        commonShader->GetVertexShaderText();
+        virtualShader = commonShader->GetVertexShaderText();
     else
-        commonShader->GetFragmentShaderText();
+        virtualShader = commonShader->GetFragmentShaderText();
 	//Tack the loaded shader to the end of the virtual shader
 	virtualShader.insert(virtualShader.end(),fileContents.begin(),fileContents.end());
 
