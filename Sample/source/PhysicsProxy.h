af07838d8e67886fe5985f821f0a72c3e1852072
wd40bomber7 <apscience@earthlink.net>
2014-03-19 11:29:11 -0700

    Physics proxies ready but still missing controlling logic. Also missing integration with the actor system

--- Sample/source/PhysicsProxy.h
+++ Sample/source/PhysicsProxy.h
@@ -7,9 +7,11 @@ class Actor;
 
 class PhysicsProxy {
 	//The actor this physics proxy is connected to
-	Actor * Effector;
+	Actor * effector;
+	vec3 * effectorPosition;
+	vec3 size;
 public:
-	PhysicsProxy();
+	PhysicsProxy(Actor * effector, vec3 * effectorPosition);
 	~PhysicsProxy();
 
 	//The full extents
@@ -18,13 +20,17 @@ public:
 	//Update Position/Velocity
 	//and zero out the acceleration afterwards
 	//apply changes to the actor this proxy is connected to
-	void Apply(float delta);
+	//returns true if this object should be destroyed
+	bool Apply(float delta);
 
 	//Check for the special case of a collision with an axis aligned voxel
 	//The fed position should be the center of the voxel
 	bool AABBCollision(vec3 voxelPosition);
 	//Check for a collision with another physicsproxy
-	bool AAABBCollision(PhysicsProxy * other);
+	bool AABBCollision(PhysicsProxy * other);
+
+	//Schedule this proxy for destruction detaching the actor automatically
+	void DetachFromEffector();
 
 	//Actors should not use these methods to enact changes (except maybe position)
 	//position of the physics proxy is the lower left
