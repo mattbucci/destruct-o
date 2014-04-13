e9e3706553874f414e2e35d71b203c6463d69351
Nathaniel Lewis <linux.robotdude@gmail.com>
2014-04-13 01:10:31 -0700

    Corrected my file headers
    Former-commit-id: b55018ae05d4ce86f2dd74e8981714634604744f

--- Sample/source/VertexAttributes.cpp
+++ Sample/source/VertexAttributes.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2014 Nathaniel Lewis, Matthew Bucci, Anthony Popelar, Brian Bamsch
+ *  Copyright 2014 Nathaniel Lewis
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
