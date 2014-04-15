9db950477a81e1131bce7bea7511869ce1e812ed
Nathaniel Lewis <linux.robotdude@gmail.com>
2014-04-14 23:20:02 -0700

    Well, since stringent checks are not in place throughout the code, might as well remove the one we did do...
    Former-commit-id: 4d5f2d712573641e27bff528098e50a599eadea8

--- Sample/source/Model.cpp
+++ Sample/source/Model.cpp
@@ -58,25 +58,15 @@ Model::~Model()
     for(std::map<Mesh *, GLuint>::iterator it = buffers.begin(); it != buffers.end(); it++)
     {
         // If the contents are still a valid vertex buffer
-        if(glIsBuffer(it->second) == GL_TRUE)
-        {
-            glDeleteBuffers(1, &it->second);
-        }
+        glDeleteBuffers(1, &it->second);
     }
     
     // Destroy all of the part render objects
     for(std::vector<Model::MeshPartRenderData *>::iterator it = renderables.begin(); it != renderables.end(); it++)
     {
         // Delete the index buffer
-        if(glIsBuffer((*it)->indices) == GL_TRUE)
-        {
-            glDeleteBuffers(1, &(*it)->indices);
-        }
-        
-        if(glIsVertexArray((*it)->attributes) == GL_TRUE)
-        {
-            glDeleteVertexArrays(1, &(*it)->attributes);
-        }
+        glDeleteBuffers(1, &(*it)->indices);
+        glDeleteVertexArrays(1, &(*it)->attributes);
         
         // Delete the object itself
         delete *it;
