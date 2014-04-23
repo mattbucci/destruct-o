1fd65d2481c058c5f1fdcfda0cc3710164e6eb61
wd40bomber7 <apscience@earthlink.net>
2014-04-23 11:41:21 -0700

    AIDS actions are now being implemented for more intelligent selection
    Former-commit-id: 053720f262b72d4bd3f660e67f0a2dd9d61e44d9

---
+++ Sample/source/AidsAction.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "AidsAction.h"
+#include "Universal.h"
+#include "BaseFrame.h"
+
+vec3 AidsAction::findHiddenLocation(vec3 startLocation, float desiredAltitude) {
+	const static float anglePart = M_PI/4.0f;
+	//Only tries 10 locations
+	//don't want to do too many ray traces 
+	const static float distancePart = 5;
+
+	vec3 lastPos;
+	ActorPlayer * player = Game()->Actors.Player();
+
+	for (int i = 0; i < 10; i++) {
+		float angle = i*anglePart;
+		float distance = pow((float)i,1.3f)*distancePart;
+		//Calculate the guess
+		vec2 twoDGuess = vec2(cos(angle)*distance,sin(angle)*distance);
+		lastPos = vec3(twoDGuess,Game()->Voxels.GetPositionHeight(twoDGuess)+desiredAltitude);
+		
+		//Check if you're obscured by fog
+		if (glm::distance(lastPos,player->GetPosition()) > Game()->ViewDistance.GetViewDistance())
+			//This point is in the fog
+			break;
+
+		PhysicsActor * actorHit;
+		if (!Universal::Trace(lastPos,glm::normalize(player->GetPosition() - lastPos),NULL,&actorHit)) {
+			//Failed to find the actor
+			if (actorHit == player)
+				continue;
+		}
+
+		//This point is good
+		break;
+	}
+	return lastPos;
+}
+
+
+//Constructing the action should start it
+AidsAction::AidsAction(vec3 actionLocation, float actionCost) {
+	this->actionCost = actionCost;
+	this->actionLocation = actionLocation;
+}
+AidsAction::~AidsAction() {
+
+}
+
+//Every subclass should have a static member GetIntensityValue()
+
+//retrieve the cost paid for doing this action
+float AidsAction::GetIntensityCost() {
+	return actionCost;
+}
---
+++ Sample/source/AidsActionDeploySoldier.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include "AidsActionDeploySoldier.h"
+#include "BaseFrame.h"
+
+#include "ActorAISoldier.h"
+
+//Constructing the action should start it
+AidsActionDeploySoldier::AidsActionDeploySoldier(vec3 actionLocation) : AidsAction(actionLocation,GetIntensityValue()) {
+	//Find a hidden location
+	vec3 deplyLocation = findHiddenLocation(actionLocation,3);
+	//Deploy
+	deployed = Game()->Actors.BuildActor<ActorAISoldier>(deplyLocation);
+}
+
+//Every subclass should have a static member GetIntensityValue()
+float AidsActionDeploySoldier::GetIntensityValue() {
+	return 10.0f;
+}
+
+
+//If this returns true clean up the action
+//the action has ended
+bool AidsActionDeploySoldier::Update() {
+	//If they're dead, they're dead
+	if (deployed->Dead())
+		return true;
+	//If they're too far away, undeploy them
+	if (glm::distance(deployed->GetPosition(),Game()->Actors.Player()->GetPosition()) > 300) {
+		deployed->Kill();
+	}
+}
