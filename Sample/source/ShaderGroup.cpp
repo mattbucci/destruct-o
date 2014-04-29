3b676abd8efd84f5a758c31600477f876cb58600
Nathaniel Lewis <linux.robotdude@gmail.com>
2014-04-29 16:18:31 -0700

    Moved the shaders out of Interface
    Former-commit-id: 0bf3c5374fcdeffcd03883a1a0ba3786c4f084c8

--- Sample/source/ShaderGroup.cpp
+++ Sample/source/ShaderGroup.cpp
@@ -10,30 +10,30 @@
 
 ShaderGroup::ShaderGroup() {
 	//Build the common shader
-	GLCommonShaderFile commonShader("Interface/Shaders/commonshader.glsl");
+	GLCommonShaderFile commonShader("shaders/commonshader.glsl");
 	//Load the shaders appropriate for the opengl version being used
 	if (OpenglVersion == 31) {
 		//Build the dialog shader
-		GL2DProgram * shaders2d = new GL2DProgram(&commonShader,"Interface/Shaders/glsl31/vsh_interface.glsl","Interface/Shaders/glsl31/fsh_interface.glsl");
+		GL2DProgram * shaders2d = new GL2DProgram(&commonShader,"shaders/glsl31/vsh_interface.glsl","shaders/glsl31/fsh_interface.glsl");
 		if (!shaders2d->Valid()) 
 			cout << "Failed to build opengl program\n";
 		AddShader(shaders2d,"2d");
         
 		//Build the voxel shader
-		GL3DProgram * shaders3d = new GL3DProgram(&commonShader,"Interface/Shaders/glsl31/vsh_3d.glsl","Interface/Shaders/glsl31/fsh_3d.glsl");
+		GL3DProgram * shaders3d = new GL3DProgram(&commonShader,"shaders/glsl31/vsh_3d.glsl","shaders/glsl31/fsh_3d.glsl");
 		if (!shaders3d->Valid()) 
 			cout << "Failed to build opengl program\n";
 		AddShader(shaders3d,"3d");
 	}
 	else {
 		//Build the dialog shader
-		GL2DProgram * shaders2d = new GL2DProgram(&commonShader,"Interface/Shaders/glsl11/vsh_interface.glsl","Interface/Shaders/glsl11/fsh_interface.glsl");
+		GL2DProgram * shaders2d = new GL2DProgram(&commonShader,"shaders/glsl11/vsh_interface.glsl","shaders/glsl11/fsh_interface.glsl");
 		if (!shaders2d->Valid()) 
 			cout << "Failed to build opengl program\n";
 		AddShader(shaders2d,"2d");
         
 		//Build the voxel shader
-		GL3DProgram * shaders3d = new GL3DProgram(&commonShader,"Interface/Shaders/glsl11/vsh_3d.glsl","Interface/Shaders/glsl11/fsh_3d.glsl");
+		GL3DProgram * shaders3d = new GL3DProgram(&commonShader,"shaders/glsl11/vsh_3d.glsl","shaders/glsl11/fsh_3d.glsl");
 		if (!shaders3d->Valid()) 
 			cout << "Failed to build opengl program\n";
 		AddShader(shaders3d,"3d");
@@ -42,31 +42,31 @@ ShaderGroup::ShaderGroup() {
 	//Build the dialog shader
 	//Build Universal Shaders
 	//Build Particle Shader
-	GLParticleProgram * shadersP = new GLParticleProgram(&commonShader,"Interface/Shaders/universal/vsh_particle.glsl","Interface/Shaders/universal/fsh_particle.glsl");
+	GLParticleProgram * shadersP = new GLParticleProgram(&commonShader,"shaders/universal/vsh_particle.glsl","shaders/universal/fsh_particle.glsl");
 	if (!shadersP->Valid()) 
 		cout << "Failed to build opengl program\n";
 	AddShader(shadersP,"particles");
 
 	//Build Terrain Shader
-	GLTerrainProgram * shadersT = new GLTerrainProgram(&commonShader,"Interface/Shaders/universal/vsh_terrain.glsl","Interface/Shaders/universal/fsh_terrain.glsl");
+	GLTerrainProgram * shadersT = new GLTerrainProgram(&commonShader,"shaders/universal/vsh_terrain.glsl","shaders/universal/fsh_terrain.glsl");
 	if (!shadersT->Valid()) 
 		cout << "Failed to build opengl program\n";
 	AddShader(shadersT,"terrain");
     
     // Build the model shader
-    MaterialProgram * modelShader = new MaterialProgram(&commonShader,"Interface/Shaders/universal/vsh_model.glsl","Interface/Shaders/universal/fsh_model.glsl");
+    MaterialProgram * modelShader = new MaterialProgram(&commonShader,"shaders/universal/vsh_model.glsl","shaders/universal/fsh_model.glsl");
     if (!modelShader->Valid())
         cout << "Failed to build opengl program (model)\n";
     AddShader(modelShader,"model");
 
     // Build the skybox shader
-    GL3DProgram * skyboxShader = new GL3DProgram(&commonShader,"Interface/Shaders/universal/vsh_skybox.glsl","Interface/Shaders/universal/fsh_skybox.glsl");
+    GL3DProgram * skyboxShader = new GL3DProgram(&commonShader,"shaders/universal/vsh_skybox.glsl","shaders/universal/fsh_skybox.glsl");
     if (!skyboxShader->Valid())
         cout << "Failed to build opengl program (skybox)\n";
     AddShader(skyboxShader,"skybox");
     
 	//Build Effect Shader
-	GLEffectProgram * shadersE = new GLEffectProgram(&commonShader,"Interface/Shaders/universal/vsh_effect.glsl","Interface/Shaders/universal/fsh_effect.glsl");
+	GLEffectProgram * shadersE = new GLEffectProgram(&commonShader,"shaders/universal/vsh_effect.glsl","shaders/universal/fsh_effect.glsl");
 	if (!shadersE->Valid()) 
 		cout << "Failed to build opengl program\n";
 	AddShader(shadersE,"effects");
