5bacff3f4356556cbe22ef5d1c8a1e2ee6a6ec35
wd40bomber7 <apscience@earthlink.net>
2014-05-08 23:48:08 -0700

    Fixed blurry title screen and spinner.
    Former-commit-id: 22e08ea72c2c0176f453947300e061f52ce6365e

--- Sample/source/GLTexture.h
+++ Sample/source/GLTexture.h
@@ -17,12 +17,25 @@ public:
 	GLTexture(string texturePathName);
 	virtual ~GLTexture();
 
+	//Flags for desired settings
+	enum textureFlags {
+		//Uses GL_LINEAR
+		TEXTURE_LINEAR = 0,
+		//uses GL_NEAREST for everything
+		TEXTURE_FASTEST = 1,
+		//Uses mipmap if possible, this is the default
+		TEXTURE_MIPMAP = 2,
+	};
+
 	//Create an error texture which relies only on memory
 	//do not attempt to cache
 	static GLTexture * GenerateErrorTexture();
 
 	//Attempt to cache the texture in graphics memory
-	virtual bool CacheTexture();
+	virtual bool CacheTexture(textureFlags flags = TEXTURE_MIPMAP);
+
+	//Default texture options
+	static const textureFlags DefaultTextureOption;
 
 	//Whether or not this texture is cached currently
 	bool IsCached();
